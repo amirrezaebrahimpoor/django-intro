cce15854492f4e9a40e087f4cb8216a5a7e20129
amirrezaebrahimpoor <amirrezaebrahimpoorcode@gmail.com>
2025-02-01 11:13:52 +0330

    second

--- api1/urls.py
+++ api1/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 import api1.views
 
 urlpatterns = [
-    path('generate-password/',api1.views.generate_password)
+    path('generate-password/',api1.views.generate_password),
+    path('random-number/',api1.views.random_number)
 ]
